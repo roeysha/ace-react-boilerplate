6db3949b02cd68206f72c7fbe76dbb612bc66ede
roeysha <roeysh@gmail.com>
2019-12-17 13:07:44 +0200

    Add react-redux

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,17 +1,24 @@
-import React, { Component} from "react";
-import {hot} from "react-hot-loader";
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import LayerManager from '@atlaskit/layer-manager';
+import rootReducer from './reducers';
 
 import "./App.css";
+
+const store = createStore(rootReducer);
 
 class App extends Component{
   render(){
     return(
-      <LayerManager>
-        <div className="App">
-          <h1>My-App</h1>
-        </div>
-      </LayerManager>
+      <Provider store={store}>
+        <LayerManager>
+          <div className="App">
+            <h1>My-App</h1>
+          </div>
+        </LayerManager>
+      </Provider>
     );
   }
 }
